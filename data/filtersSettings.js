0884376ff7d80ea9a5d5410796ec850b830d9908
Anthony Gómez <anGpersonal@outlook.com>
2023-07-07 00:42:28 -0400

    job data

---
+++ data/filtersSettings.js
@@ -0,0 +1,179 @@
+// Array de puestos de trabajo
+const puestosDeTrabajo = [
+    {
+      id: 1,
+      title: "Desarrollador de software",
+      job_description: "Responsable de desarrollar y mantener aplicaciones web.",
+      salary: 5000,
+      company: "TechSolutions Inc.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 2,
+      title: "Diseñador gráfico",
+      job_description: "Encargado de crear diseños visuales atractivos para diversos proyectos.",
+      salary: 4000,
+      company: "Creative Design Agency",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 3,
+      title: "Gerente de proyecto",
+      job_description: "Encargado de liderar y coordinar proyectos de principio a fin.",
+      salary: 6000,
+      company: "Project Management Solutions",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 4,
+      title: "Especialista en marketing digital",
+      job_description: "Responsable de desarrollar estrategias de marketing en línea.",
+      salary: 4500,
+      company: "Digital Marketing Experts",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 5,
+      title: "Analista financiero",
+      job_description: "Encargado de analizar datos financieros y realizar informes.",
+      salary: 5500,
+      company: "Financial Insights Group",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 6,
+      title: "Ingeniero de software",
+      job_description: "Responsable de diseñar, desarrollar y probar software.",
+      salary: 5200,
+      company: "Software Solutions Ltd.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 7,
+      title: "Especialista en recursos humanos",
+      job_description: "Encargado de gestionar el reclutamiento y el desarrollo del personal.",
+      salary: 4800,
+      company: "Human Capital Group",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 8,
+      title: "Arquitecto de soluciones",
+      job_description: "Responsable de diseñar y desarrollar arquitecturas de software.",
+      salary: 5800,
+      company: "Solution Architects Inc.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 9,
+      title: "Especialista en soporte técnico",
+      job_description: "Encargado de brindar asistencia y solución de problemas técnicos.",
+      salary: 4200,
+      company: "Technical Support Services",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 10,
+      title: "Consultor de negocios",
+      job_description: "Responsable de proporcionar asesoramiento y soluciones empresariales.",
+      salary: 5500,
+      company: "Business Consultants Ltd.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 11,
+      title: "Desarrollador de aplicaciones móviles",
+      job_description: "Encargado de desarrollar aplicaciones móviles para plataformas iOS y Android.",
+      salary: 5200,
+      company: "Mobile App Solutions",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 12,
+      title: "Analista de datos",
+      job_description: "Responsable de analizar grandes volúmenes de datos y extraer información.",
+      salary: 6000,
+      company: "Data Analytics Experts",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 13,
+      title: "Diseñador de experiencia de usuario",
+      job_description: "Encargado de diseñar interfaces de usuario intuitivas y atractivas.",
+      salary: 4800,
+      company: "UX Design Studios",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 14,
+      title: "Desarrollador de back-end",
+      job_description: "Responsable de desarrollar la lógica de negocio en el lado del servidor.",
+      salary: 5200,
+      company: "Back-end Solutions Ltd.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 15,
+      title: "Analista de seguridad informática",
+      job_description: "Encargado de garantizar la seguridad de los sistemas informáticos.",
+      salary: 5500,
+      company: "Cybersecurity Consultants",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 16,
+      title: "Especialista en marketing de contenidos",
+      job_description: "Responsable de crear y gestionar estrategias de marketing de contenidos.",
+      salary: 4200,
+      company: "Content Marketing Agency",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 17,
+      title: "Administrador de bases de datos",
+      job_description: "Encargado de administrar y mantener bases de datos eficientes y seguras.",
+      salary: 5800,
+      company: "Database Management Solutions",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 18,
+      title: "Especialista en recursos humanos",
+      job_description: "Encargado de desarrollar políticas y programas de recursos humanos.",
+      salary: 4800,
+      company: "HR Strategies Ltd.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 19,
+      title: "Ingeniero de software",
+      job_description: "Responsable de desarrollar soluciones de software escalables y eficientes.",
+      salary: 5200,
+      company: "Software Engineering Inc.",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 20,
+      title: "Analista de negocio",
+      job_description: "Encargado de analizar los procesos empresariales y proponer mejoras.",
+      salary: 5500,
+      company: "Business Analysis Services",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 21,
+      title: "Desarrollador de aplicaciones web",
+      job_description: "Responsable de desarrollar aplicaciones web interactivas y eficientes.",
+      salary: 5200,
+      company: "Web App Solutions",
+      created_date: "2023-07-07"
+    },
+    {
+      id: 22,
+      title: "Especialista en análisis de datos",
+      job_description: "Encargado de analizar y visualizar datos para obtener información valiosa.",
+      salary: 6400,
+      company: "Business Analy",
+      created_date: "2023-07-07"
+    }
+]
